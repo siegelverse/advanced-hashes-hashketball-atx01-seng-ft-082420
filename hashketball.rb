815e3f1ac81fa6e5ea1fe1e1d07c7528826dd6c7
Marcus Siegel <msiegel37@gmail.com>
2020-08-14 22:59:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -170,6 +170,7 @@ def player_number(team_name)
   jersey_numbers = []
   game_hash.each do |team, team_data|
     if team_data[:team_name] == team_name 
-      team_data[:players].each do ||
+      team_data[:players].each do |stats|
+        
     
 # Write code here
